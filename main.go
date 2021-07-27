4585b4f721885eb33fc4e0def3616f066d4c5b24
xujin <1319039722@qq.com>
2021-07-27 10:58:12 +0800

    [feat] add simple rep function

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func READ() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	return text, nil
+}
+
+func EVAL(text string) string {
+	return text
+}
+
+func PRINT(text string) {
+	fmt.Print(text)
+}
+
+func rep() {
+	text, _ := READ()
+	res := EVAL(text)
+	PRINT(res)
+}
+
 func main() {
-	print("hello,world")
+	for {
+		fmt.Print("user> ")
+		rep()
+	}
 }
