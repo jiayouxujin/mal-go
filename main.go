d783ba56287cd4bbb237209cbab3fa72b3a40dda
xujin <1319039722@qq.com>
2021-07-28 18:01:59 +0800

    bugfix: let*

--- main.go
+++ main.go
@@ -55,65 +55,64 @@ func evalAst(ast MalType, env *env.Env) (MalType, error) {
 	}
 }
 func EVAL(ast MalType, e *env.Env) (MalType, error) {
-	for{
-		switch t := ast.(type) {
-		case MalList:
-			if len(t) == 0 {
-				return t, nil //ast is empty list return ast unchanged
-			}
-			first := ""
-			if symbol, ok := t[0].(MalSymbol); ok {
-				first = symbol.Value
-			}
-			switch first {
-			case "def!":
-				if len(t) != 3 {
-					return nil, fmt.Errorf("incorrect number of parameters for 'def!'")
-				}
-				k, ok := t[1].(MalSymbol)
-				if !ok {
-					return nil, fmt.Errorf("the first parameter is expected to be a symbol")
-				}
-				v, err := EVAL(t[2], e)
-				if err != nil {
-					return nil, err
-				}
-				err = e.Set(k, v)
-				return v, err
-			case "let*":
-				if len(t) != 3 {
-					return nil, fmt.Errorf("incorrect number of arguments for 'let*'")
-				}
-				bindings, ok := t[1].(MalList)
-				if !ok || len(bindings)%2 != 0 {
-					return nil, fmt.Errorf("the first parameter is expected to be a list of even length")
-				}
-				tmpEnv, _ := env.CreateEnv(e, nil, nil)
-				for i := 0; i < len(bindings); i += 2 {
-					k, ok := bindings[i].(MalSymbol)
-					if !ok {
-						return nil, fmt.Errorf("invalid symbol(s) in variable bindings")
-					}
-					v, err := EVAL(bindings[i+1], tmpEnv)
-					if err != nil {
-						return nil, err
-					}
-					err = tmpEnv.Set(k, v)
-					if err != nil {
-						return nil, err
-					}
-				}
-				ast, e = t[2], tmpEnv
-			default:
-				evaluatedList, err := evalAst(t, e)
-				if err != nil {
-					return nil, err
-				}
-				return evaluatedList.(MalList)[0].(MalFunction)(evaluatedList.(MalList)[1:]...)
-			}
-		default: //ast is not a list,call evalAst
-			return evalAst(ast, e)
+	switch t := ast.(type) {
+	case MalList:
+		if len(t) == 0 {
+			return t, nil //ast is empty list return ast unchanged
 		}
+		first := ""
+		if symbol, ok := t[0].(MalSymbol); ok {
+			first = symbol.Value
+		}
+		switch first {
+		case "def!":
+			if len(t) != 3 {
+				return nil, fmt.Errorf("incorrect number of parameters for 'def!'")
+			}
+			k, ok := t[1].(MalSymbol)
+			if !ok {
+				return nil, fmt.Errorf("the first parameter is expected to be a symbol")
+			}
+			v, err := EVAL(t[2], e)
+			if err != nil {
+				return nil, err
+			}
+			err = e.Set(k, v)
+			return v, err
+		case "let*":
+			if len(t) != 3 {
+				return nil, fmt.Errorf("incorrect number of arguments for 'let*'")
+			}
+			bindings, ok := t[1].(MalList)
+			if !ok || len(bindings)%2 != 0 {
+				return nil, fmt.Errorf("the first parameter is expected to be a list of even length")
+			}
+			tmpEnv, _ := env.CreateEnv(e, nil, nil)
+			for i := 0; i < len(bindings); i += 2 {
+				k, ok := bindings[i].(MalSymbol)
+				if !ok {
+					return nil, fmt.Errorf("invalid symbol(s) in variable bindings")
+				}
+				v, err := EVAL(bindings[i+1], tmpEnv)
+				if err != nil {
+					return nil, err
+				}
+				err = tmpEnv.Set(k, v)
+				if err != nil {
+					return nil, err
+				}
+			}
+			ast, e = t[2], tmpEnv
+			return EVAL(ast, e)
+		default:
+			evaluatedList, err := evalAst(t, e)
+			if err != nil {
+				return nil, err
+			}
+			return evaluatedList.(MalList)[0].(MalFunction)(evaluatedList.(MalList)[1:]...)
+		}
+	default: //ast is not a list,call evalAst
+		return evalAst(ast, e)
 	}
 }
 
